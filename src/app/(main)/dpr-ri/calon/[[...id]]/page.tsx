b9eac4144623217925d3f2f7e55d031eebf3bf95
sauki futaki <utaki.sauki19@gmail.com>
2024-02-12 22:07:42 +0700

    kedua kaykanya

--- src/app/(main)/dpr-ri/calon/[[...id]]/page.tsx
+++ src/app/(main)/dpr-ri/calon/[[...id]]/page.tsx
@@ -1,14 +1,18 @@
 "use client"
 import useSWR from 'swr'
+
+
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 interface Page {
   params: {
-    id: string[] 
+    id: string
   }
 }
 export default function Page({params}: Page){
+
   const {id} = params
+  
   const {data, error} = useSWR(`https://caleg.zakiego.com/api/dpr-ri/calon/${id}}`, fetcher)
   if(!data) return <div>Loading...</div>
   if(error) return <div>{error.message}</div>
